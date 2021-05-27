5530b8aa1d8f2c1db01aa49f82bbf3a29ad89369
KylianChasselin <74654924+KylianChasselin@users.noreply.github.com>
2021-05-27 15:15:14 +0200

    base

---
+++ assets/branche.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="branche" tilewidth="32" tileheight="32" tilecount="336" columns="28">
+ <image source="../asset jungle tronc.png" width="900" height="400"/>
+</tileset>
